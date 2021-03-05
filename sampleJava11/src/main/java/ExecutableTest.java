5db2f621acd60508f9227b67c7dc00437a90af3c
vinub <vinubunny@gmail.com>
2021-03-05 13:56:28 -0700

    initial commit

---
+++ sampleJava11/src/main/java/ExecutableTest.java
@@ -0,0 +1,12 @@
+public class ExecutableTest {
+
+  public static void main(String[] args) {
+    
+    Integer l1 = Integer.valueOf(1000);
+    Integer l2 = Integer.valueOf(1000);
+
+    System.out.println(l1 == l2);
+
+  }
+
+}
